0afe6c4e791a2acfc929b8c94078d480ea304784
nkt.a3 <test@test>
2015-11-20 00:39:23 +0000

    Mailer & Integration_Test

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "世紀末覇者 拳王 <info@depot.com>"
+  layout 'mailer'
+end
---
+++ test/integration/user_stories_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class UserStoriesTest < ActionDispatch::IntegrationTest
+  fixtures :products
+
+  test "buying a product" do
+    LineItem.delete_all
+    Order.delete_all
+    ruby_book = products(:ruby)
+
+    get "/"
+    assert_response :success
+    assert_template "index"
+
+    post_via_redirect "/line_items", product_id: ruby_book.id
+    assert_response :success
+    assert_template "show"
+
+    cart = Cart.find(session[:cart_id])
+    assert_equal 1, cart.line_items.size
+    assert_equal ruby_book, cart.line_items[0].product
+
+    get "/orders/new"
+    assert_response :success
+    assert_template "new"
+
+    post_via_redirect "/orders",
+                      order: { name:          "Dave Thomas",
+                               address:       "123 the street",
+                               email:         "dave@example.com",
+                               pay_type:      "現金" }
+
+    assert_response :success
+    assert_template "index"
+    cart = Cart.find(session[:cart_id])
+    assert_equal 0, cart.line_items.size
+
+    orders = Order.all
+    assert_equal 1, orders.size
+    order = orders[0]
+
+  end
+end
