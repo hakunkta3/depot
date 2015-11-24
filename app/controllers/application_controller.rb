8fd9f8a37166b05906860d15ee8ea7a0fa6e413a
nkt.a3 <test@test>
2015-11-24 00:36:07 +0000

    　Login!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :now#, if: :whilelist
+  before_action :authorize
 
   def now
       @time = Time.now
   end
 
   private
+
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "ログインして下さい"
+    end
+  end
 
   def whilelist
     %w{store products carts}.include?(controller_name)
